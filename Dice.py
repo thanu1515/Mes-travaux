997e156f83bb488d0c9723be7dec7356194ac79a
thanu1515 <140270434+thanu1515@users.noreply.github.com>
2023-08-09 20:54:55 +0530

    Dice.py

--- Dice.py
+++ Dice.py
@@ -1,15 +1,12 @@
-# import modules
 import turtle
 import random
   
-# creating the screen
 d = turtle.Screen()
 d.setup(width=300, height=300)
 d.bgcolor('white')
 d.title('Dice Play')
 d.tracer(1)
-  
-# creating the dots
+
 dot_positions = [[(0, 0, 'red'), (-100, 100, 'white'), (-100, 0, 'white'), (-100, -100, 'white'), (100, 100, 'white'),
                   (100, 0, 'white'), (100, -100, 'white')],
   
@@ -33,7 +30,7 @@ dot = [turtle.Turtle() for _ in range(7)]
   
   
 def click():
-  # generating random number
+  
     num = random.randint(1, 6)
   
     print(f" The number rolled on dice is {num}\n ")
